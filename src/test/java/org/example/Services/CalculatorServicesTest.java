f1d64a12c59729cbfe416921c4ef3e5b98cb552a
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2025-01-10 19:04:20 +0530

    juint 4 ended and 5 started!

--- src/test/java/org/example/Services/CalculatorServicesTest.java
+++ src/test/java/org/example/Services/CalculatorServicesTest.java
@@ -1,9 +1,24 @@
 package org.example.Services;
 
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 public class CalculatorServicesTest {
+
+    @BeforeClass
+    public static void init(){
+        System.out.println("Before all test cases");
+        System.out.println("started Test : " +new Date());
+    }
+
+    public static void cleanup(){
+        System.out.println("After all test case");
+        System.out.println("End tst cases"+ new Date());
+    }
+
 
     CalculatorServices calculator = new CalculatorServices();
 
